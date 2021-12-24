d0b415882dfb7d2ee6171c68cc11569db9396e1b
joseluisaguilar-cognizant <JoseLuis.AguilarGomez@cognizant.com>
2021-12-24 11:44:27 +0100

    Set defaultProyect: TS, SCSS, and base component

---
+++ src/modules/dashboard/components/Header/Header.tsx
@@ -0,0 +1,29 @@
+import { CSSProperties, FunctionComponent } from 'react';
+
+import './Header.scss';
+
+interface IHeader {
+  text: string;
+  bgColor?: string;
+  textColor?: string;
+}
+
+const Header: FunctionComponent<IHeader> = ({
+  text = 'Default title',
+  bgColor = 'rgba(0,0,0,0.4)',
+  textColor = '#ff6a95',
+}) => {
+  const headerStyles: CSSProperties = {
+    color: textColor,
+    backgroundColor: bgColor,
+  };
+
+  return (
+    <header className='header' style={headerStyles}>
+      <div className='container'>
+        <h2>{text}</h2>
+      </div>
+    </header>
+  );
+};
+export default Header;
